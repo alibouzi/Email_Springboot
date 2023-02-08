c979180dad2e4c5f4b2c149a6eb3a790ba05a4ca
unknown <TEC-ALI-BOUZID@tech-alibouzid.MSS.local>
2023-02-08 20:40:03 +0100

    Email Sender

---
+++ src/main/java/com/example/mailing/EmailSenderService.java
@@ -0,0 +1,26 @@
+package com.example.mailing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class EmailSenderService {
+    @Autowired
+    private JavaMailSender mailSender;
+    public  void sendEmail(String toEmail,
+                           String subject,
+                           String body){
+        SimpleMailMessage message =new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+        message.setSentDate(new Date());
+        mailSender.send(message);
+
+        System.out.println("Mail Send Succsfully...");
+    }
+}
